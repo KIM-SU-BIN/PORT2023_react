d4ae75bb3f443705b897f3a7486016e7a4771e40
kimsubin <subintoto@sensemeka.com>
2025-02-13 00:36:19 +0900

    인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,38 +2,43 @@ import React from "react"
 
 import about from "../assets/img/about.jpg";
 
+const introText = {
+    title: "port developer",
+    desc: ["talent is", "found at the end of the", "effort"]
+}
+
 const Intro = () => {
     return (
         <section id="intro">
-            <div class="intro__inner">
-                <h1 class="intro__title">port developer</h1>
-                <div class="intro__lines" aria-hidden="true">
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
+            <div className="intro__inner">
+                <h1 className="intro__title">{introText.title}</h1>
+                <div className="intro__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
                 </div>
-                <div class="intro__text">
-                    <div class="text">
-                        <div>talent is</div>
-                        <div>found at the end of the</div>
-                        <div>effort</div>
+                <div className="intro__text">
+                    <div className="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
                     </div>
-                    <div class="img">
+                    <div className="img">
                         <img src={about} alt="어바웃미" />
                     </div>
                 </div>
-                <div class="intro__lines bottom" aria-hidden="true">
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
+                <div className="intro__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
                 </div>
             </div>
         </section>
